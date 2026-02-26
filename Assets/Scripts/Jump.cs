ed40bdc5c8ac88c417927377412d15e2875b4fe9
Siarhei <siarhei.it.79@gmail.com>
2026-02-26 17:27:00 +0300

    added jump script

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("Jump");
+    }
+}
